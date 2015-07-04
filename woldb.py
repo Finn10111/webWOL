84c869971f97b5171bb4b388f4c351146ba43067
Finn Christiansen <kontakt@finnchristiansen.de>
2015-07-04 10:12:21 +0200

    Added woldb.py

---
+++ woldb.py
@@ -0,0 +1,23 @@
+import config
+
+db = config.db
+
+
+def get_hosts():
+    return db.select('host', order='id')
+
+
+def get_host(id):
+    return db.select('host', where="id=$id", vars=locals())[0]
+
+
+def add_host(name, mac, ip):
+    db.insert('host', name=name, mac=mac, ip=ip)
+
+
+def delete_host(id):
+    db.delete('host', where="id=$id", vars=locals())
+
+
+def update_host(id, name, mac, ip):
+    db.update('host', where="id=$id", vars=locals(), name=name, mac=mac, ip=ip)
